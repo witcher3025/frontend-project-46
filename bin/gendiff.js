49161341429275ecba9dead879eb09d31de94be6
witcher3025 <a.avsetov@outlook.com>
2023-06-07 15:14:24 +0300

    add bin/gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference')
+  .version('1.0.0')
+
+program.parse();
