50cdfb1e5825e66d6dddaad398f8fe169b4d27a3
witcher3025 <a.avsetov@outlook.com>
2023-06-07 15:33:56 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --style <type>', 'output format');
 
 program.parse();
