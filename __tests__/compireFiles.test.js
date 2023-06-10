21d8934c05fa7010b8246b4b89c9c66eb4fb9638
witcher3025 <a.avsetov@outlook.com>
2023-06-10 15:52:19 +0300

    fix tests

--- __tests__/compireFiles.test.js
+++ __tests__/compireFiles.test.js
@@ -4,10 +4,18 @@ import * as fs from 'node:fs';
 // import _ from 'lodash';
 import compareFiles from '../src/compareFiles.js';
 
-const absolutePath1 = './__fixtures__/files/file1.json';
-const absolutePath2 = './__fixtures__/files/file2.json';
+const filepath1 = './__fixtures__/files/file1.json';
+const filepath2 = './__fixtures__/files/file2.json';
 const filepath3 = '__fixtures__/files/file3.json';
 const filepath4 = '__fixtures__/files/file4.json';
+
+const cases = [
+  [0, filepath1, filepath2],
+  [1, filepath2, filepath4],
+  [2, filepath4, filepath2],
+  [3, filepath1, filepath3],
+  [4, filepath1, filepath1],
+];
 
 const expectedData = { plain: [] };
 
@@ -16,37 +24,11 @@ beforeAll(() => {
   expectedData.plain = plainData.split('###').map((str) => str.trim());
 });
 
-test('diff between files 1-2', () => {
-  const caseIndex = 0;
-  const actual = compareFiles(absolutePath1, absolutePath2);
-  const expected = expectedData.plain[caseIndex];
-  expect(actual).toEqual(expected);
-});
+describe.each(cases)('', (caseIndex, file1, file2) => {
+  test(`diff ${caseIndex + 1}`, () => {
+    const actual = compareFiles(file1, file2);
+    const expected = expectedData.plain[caseIndex];
 
-test('diff between files 2-4', () => {
-  const caseIndex = 1;
-  const actual = compareFiles(absolutePath2, filepath4);
-  const expected = expectedData.plain[caseIndex];
-  expect(actual).toEqual(expected);
-});
-
-test('diff between files 4-2', () => {
-  const caseIndex = 2;
-  const actual = compareFiles(filepath4, absolutePath2);
-  const expected = expectedData.plain[caseIndex];
-  expect(actual).toEqual(expected);
-});
-
-test('diff between files 1-3', () => {
-  const caseIndex = 3;
-  const actual = compareFiles(absolutePath1, filepath3);
-  const expected = expectedData.plain[caseIndex];
-  expect(actual).toEqual(expected);
-});
-
-test('diff between same files 1-1', () => {
-  const caseIndex = 4;
-  const actual = compareFiles(absolutePath1, absolutePath1);
-  const expected = expectedData.plain[caseIndex];
-  expect(actual).toEqual(expected);
+    expect(actual).toEqual(expected);
+  });
 });
