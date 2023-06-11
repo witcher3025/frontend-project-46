b7abceb6fdb63927bf5ba93aacd1b2d2dfcdbbcf
witcher3025 <109389798+witcher3025@users.noreply.github.com>
2023-06-11 13:00:26 +0300

    update test and add CI

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,7 +1,14 @@
 /* eslint-disable no-console, import/extensions */
 
 import * as fs from 'node:fs';
+import * as path from 'node:path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const filepath1 = './__fixtures__/files/file1.json';
 const filepath2 = './__fixtures__/files/file2.json';
@@ -19,7 +26,7 @@ const cases = [
 const expectedData = { plain: [] };
 
 beforeAll(() => {
-  const plainData = fs.readFileSync(('__fixtures__/data.txt'), 'utf-8');
+  const plainData = readFile('data.txt');
   expectedData.plain = plainData.split('###').map((str) => str.trim());
 });
 
