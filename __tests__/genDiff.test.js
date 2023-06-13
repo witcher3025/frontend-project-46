f8546126501f34e207ada0e0a7c0960cc5b18a5b
witcher3025 <109389798+witcher3025@users.noreply.github.com>
2023-06-13 13:05:41 +0300

    refactoring test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -10,30 +10,50 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const filepath1 = './__fixtures__/files/file1.json';
-const filepath2 = './__fixtures__/files/file2.json';
-const filepath3 = '__fixtures__/files/file3.yaml';
-const filepath4 = '__fixtures__/files/file4.yml';
+const plainFilepath1 = './__fixtures__/files_plane/file1.json';
+const plainFilepath2 = './__fixtures__/files_plane/file2.json';
+const plainFilepath3 = '__fixtures__/files_plane/file3.yaml';
+const plainFilepath4 = '__fixtures__/files_plane/file4.yml';
 
-const cases = [
-  [0, filepath1, filepath2],
-  [1, filepath2, filepath4],
-  [2, filepath4, filepath2],
-  [3, filepath1, filepath3],
-  [4, filepath1, filepath1],
+const plainCases = [
+  [0, plainFilepath1, plainFilepath2],
+  [1, plainFilepath2, plainFilepath4],
+  [2, plainFilepath4, plainFilepath2],
+  [3, plainFilepath1, plainFilepath3],
+  [4, plainFilepath1, plainFilepath1],
 ];
 
-const expectedData = { plain: [] };
+const nestedFilepath1 = './__fixtures__/files_nested/file1.json';
+const nestedFilepath2 = './__fixtures__/files_nested/file2.json';
+const nestedFilepath3 = '__fixtures__/files_nested/file3.yml';
+
+const nestedCases = [
+  [0, nestedFilepath1, nestedFilepath2],
+  [1, nestedFilepath1, nestedFilepath3],
+];
+
+const expectedData = { nested: [], plain: [] };
 
 beforeAll(() => {
-  const plainData = readFile('data.txt');
+  const plainData = readFile('plane.txt');
+  const nestedData = readFile('nested.txt');
   expectedData.plain = plainData.split('###').map((str) => str.trim());
+  expectedData.nested = nestedData.split('###').map((str) => str.trim());
 });
 
-describe.each(cases)('', (caseIndex, file1, file2) => {
-  test(`diff ${caseIndex + 1}`, () => {
+describe.each(plainCases)('', (caseIndex, file1, file2) => {
+  test(`plain diff ${caseIndex + 1}`, () => {
     const actual = genDiff(file1, file2);
     const expected = expectedData.plain[caseIndex];
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe.each(nestedCases)('', (caseIndex, file1, file2) => {
+  test(`nested diff ${caseIndex + 1}`, () => {
+    const actual = genDiff(file1, file2);
+    const expected = expectedData.nested[caseIndex];
 
     expect(actual).toEqual(expected);
   });
