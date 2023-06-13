aa1aaa4e8c2a83d0a4b2a851d5a3f00ccf2fe1e4
witcher3025 <109389798+witcher3025@users.noreply.github.com>
2023-06-13 20:48:18 +0300

    refactoring code in src/compareFiles.js

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -6,28 +6,31 @@ const compareFiles = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(keys);
 
-  const compareKeys = sortedKeys.reduce((acc, key) => {
-    const newAcc = [...acc];
+  const compareKeys = sortedKeys.map((key) => {
     if (!Object.hasOwn(data1, key)) {
       const node = { key, status: 'added', value: data2[key] };
-      newAcc.push(node);
-    } else if (!Object.hasOwn(data2, key)) {
-      const node = { key, status: 'removed', value: data1[key] };
-      newAcc.push(node);
-    } else if (data1[key] === data2[key]) {
-      const node = { key, status: 'unchanged', value: data1[key] };
-      newAcc.push(node);
-    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      const value = compareFiles(data1[key], data2[key]);
-      const node = { key, status: 'complex', value };
-      newAcc.push(node);
-    } else {
-      const value = { newValue: data2[key], oldValue: data1[key] };
-      const node = { key, status: 'updated', value };
-      newAcc.push(node);
+      return node;
     }
 
-    return newAcc;
+    if (!Object.hasOwn(data2, key)) {
+      const node = { key, status: 'removed', value: data1[key] };
+      return node;
+    }
+
+    if (data1[key] === data2[key]) {
+      const node = { key, status: 'unchanged', value: data1[key] };
+      return node;
+    }
+
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      const value = compareFiles(data1[key], data2[key]);
+      const node = { key, status: 'complex', value };
+      return node;
+    }
+
+    const value = { newValue: data2[key], oldValue: data1[key] };
+    const node = { key, status: 'updated', value };
+    return node;
   }, []);
 
   return compareKeys;
