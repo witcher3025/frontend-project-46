87b09774a495024ef02144bb8a024a3d56f59e60
witcher3025 <109389798+witcher3025@users.noreply.github.com>
2023-06-13 19:56:36 +0300

    update tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,57 +5,21 @@ import * as path from 'node:path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFilePath = (filename) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.join(__dirname, '..', '__fixtures__', filename);
+};
 
-const filepath1 = './__fixtures__/files/file1.json';
-const filepath2 = './__fixtures__/files/file2.json';
-const filepath3 = '__fixtures__/files/file3.yml';
+const readFile = (filename) => fs.readFileSync(getFilePath(filename), 'utf-8');
 
-const cases = [
-  [0, filepath1, filepath2],
-  [1, filepath1, filepath3],
-];
+describe.each(['stylish', 'plain', 'json'])('output format %s', (format) => {
+  const result = readFile(`${format} data.txt`);
 
-let jsonCase;
-let plainCase;
-let stylishCase;
+  test.each(['json', 'yaml'])('file %s', (ext) => {
+    const filename1 = getFilePath(`file1.${ext}`);
+    const filename2 = getFilePath(`file2.${ext}`);
 
-beforeAll(() => {
-  const jsonData = readFile('json data.txt');
-  const plainData = readFile('plain data.txt');
-  const stylishData = readFile('stylish data.txt');
-
-  jsonCase = jsonData.split('###').map((str) => str.trim());
-  plainCase = plainData.split('###').map((str) => str.trim());
-  stylishCase = stylishData.split('###').map((str) => str.trim());
-});
-
-describe.each(cases)('stylish format', (caseIndex, file1, file2) => {
-  test(`diff stylish ${caseIndex + 1}`, () => {
-    const actual = genDiff(file1, file2, 'stylish');
-    const expected = stylishCase[caseIndex];
-
-    expect(actual).toEqual(expected);
-  });
-});
-
-describe.each(cases)('plain format', (caseIndex, file1, file2) => {
-  test(`diff plain ${caseIndex + 1}`, () => {
-    const actual = genDiff(file1, file2, 'plain');
-    const expected = plainCase[caseIndex];
-
-    expect(actual).toEqual(expected);
-  });
-});
-
-describe.each(cases)('json format', (caseIndex, file1, file2) => {
-  test(`diff json ${caseIndex + 1}`, () => {
-    const actual = genDiff(file1, file2, 'json');
-    const expected = jsonCase[caseIndex];
-
-    expect(actual).toEqual(expected);
+    expect(genDiff(filename1, filename2, format)).toEqual(result);
   });
 });
