7ef3e54bf71c372af2f3b456a2784f13b99d5981
witcher3025 <109389798+witcher3025@users.noreply.github.com>
2023-06-13 16:03:39 +0300

    refactoring compareFiles.js

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -3,9 +3,10 @@
 import _ from 'lodash';
 
 const compareFiles = (data1, data2) => {
-  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  const keys = _.union(Object.keys(data1), Object.keys(data2));
+  const sortedKeys = _.sortBy(keys);
 
-  const compareKeys = keys.map((key) => {
+  const compareKeys = sortedKeys.map((key) => {
     const node = { key };
 
     if (!Object.hasOwn(data1, key)) {
@@ -13,16 +14,16 @@ const compareFiles = (data1, data2) => {
       node.status = 'added';
     } else if (!Object.hasOwn(data2, key)) {
       node.value = data1[key];
-      node.status = 'deleted';
+      node.status = 'removed';
     } else if (data1[key] === data2[key]) {
       node.value = data1[key];
       node.status = 'unchanged';
     } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       node.value = compareFiles(data1[key], data2[key]);
-      node.status = 'nested';
+      node.status = 'complex';
     } else {
       node.value = { oldValue: data1[key], newValue: data2[key] };
-      node.status = 'changed';
+      node.status = 'updated';
     }
 
     return node;
